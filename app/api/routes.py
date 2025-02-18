939c8ea6d414a364114eb3c7b6ffb00a3c095842
Bruno Alencar <alencarb98@gmail.com>
2025-02-18 19:48:53 -0300

    feat: receive messages from whatsapp through webhook

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,3 +2,10 @@ from typing import Any, Dict
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.post("/webhook/whatsapp")
+async def whatsapp_webhook(payload: Dict[str, Any]):
+    """
+        Webhook para receber mensagens do Whatsapp
+    """
+    ...
